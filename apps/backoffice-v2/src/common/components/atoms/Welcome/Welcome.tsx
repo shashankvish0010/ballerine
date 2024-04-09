3d1ae2e10fea6a65684196c529e628d12db2821a
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-04-09 16:51:48 +0530

    Changes: Updated the useMemo to return an object with user specific details.

--- apps/backoffice-v2/src/common/components/atoms/Welcome/Welcome.tsx
+++ apps/backoffice-v2/src/common/components/atoms/Welcome/Welcome.tsx
@@ -5,13 +5,16 @@ import { UserAvatar } from '../../atoms/UserAvatar/UserAvatar';
 
 export const Welcome: FunctionComponent = () => {
   const { data: session } = useAuthenticatedUserQuery();
-  username = useMemo(() => `${session?.user?.firstName}`, [session?.user?.firstName]);
+  const user = useMemo(() => ({
+    firstname: session?.user?.firstName,
+    avatarUrl: session?.user?.avatarUrl
+  }), [session?.user?.firstName, session?.user?.avatarUrl]);
   return (
     <div className={`mt-[27px] flex h-[36px] w-[441px] items-center`}>
-      <UserAvatar className={`mr-2 d-6`} avatarUrl={session?.user?.avatarUrl} />
+     {user.avatarUrl && <UserAvatar className={`mr-2 d-6`} avatarUrl={user.avatarUrl} />}
       <div className={`flex gap-x-2 text-[24px] font-semibold leading-[36px]`}>
         <span>{t('welcome.greeting', { defaultValue: 'Welcome' })}</span>
-        <span>{username}</span>
+        {user.firstname && <span>{user.firstname}</span>}
       </div>
     </div>
   );
